58e43791a04e636f759822655596c8d1d85bd1f0
Allan <allan.landin25@gmail.com>
2023-12-09 15:36:31 -0300

    Initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 const form = document.querySelector("#form1")
 const mainDiv = document.querySelector("#main");
+const divResult = document.querySelector("#result");
+const buttonReset = document.querySelector("#cleanBtn")
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -8,22 +10,36 @@ form.addEventListener("submit", (e)=>{
         dateReference: form[0].value,
         minusOperation: form[1].checked,
         sumOperation: form[2].checked,
-        months: form[3].value,
-        considerUtilsDays: form[4].value
+        qtDays: parseInt(form[3].value) + 1,
     }
-
+    console.log(formData);
     // verify the type of operation
-    if (minusOperation){
-        calculateDatePrev(dateReference, months, considerUtilsDays);
+    let result = ""
+    
+    if (formData.minusOperation){
+        result = calculateDatePrev(formData.dateReference, formData.qtDays);
+    } else{
+        result = calculateDateAfter(formData.dateReference, formData.qtDays);
     }
-    calculateDateAfter(dateReference, months, considerUtilsDays);
+    if (result === "Invalid Date"){
+        divResult.innerHTML = "Erro! Coloque uma data válida!"
+        return;
+    }
+    console.log(result)
+    divResult.innerHTML = result
 })
 
-function calculateDatePrev(dateReference, months, considerUtilsDays){
-    if (considerUtilsDays == "on"){
-        //    
-    }
-    
+function calculateDatePrev(dateReference, qtDays){
+    let date = new Date(dateReference)
+    date.setDate(date.getDate() - qtDays)
+    return date.toLocaleDateString("pt-BR");
 }
-function calculateDateAfter(dateReference, months, considerUtilsDays){
+function calculateDateAfter(dateReference, qtDays){
+    let date = new Date(dateReference)
+    date.setDate(date.getDate() + qtDays)
+    return date.toLocaleDateString("pt-BR");
 }
+
+buttonReset.addEventListener("click", ()=>{
+    divResult.innerHTML = ""
+})
